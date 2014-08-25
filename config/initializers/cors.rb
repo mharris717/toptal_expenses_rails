79d70730b073983051248b6bff498b0f4193d347
Mike Harris <mharris717@gmail.com>
2014-08-25 14:58:58 -0400

    CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use Rack::Cors do
   allow do
     origins '*'
-    resource '*', :headers => :any, :methods => [:get, :post, :options]
+    resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
   end
 end
